07a591311b200cbecb808fc26fb76c87acf9cfe2
Tobias <tobiasramossilva@gmail.com>
2025-06-10 16:48:16 -0300

    Adicionando o repositório

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "TaskSphere",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
